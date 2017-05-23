550ee7c8a1334df373ce055876b4f65241ea68da
chuckzhang <345804606@qq.com>
2017-05-23 19:58:13 +0800

    Modify cache_test.go add func TestExists

--- cache_test.go
+++ cache_test.go
@@ -41,3 +41,36 @@ func TestCache(t *testing.T){
 		t.Error("Error getting creation time")
 	}
 }
+
+func TestCacheExpire(t *testing.T) {
+	table := Cache("testCache")
+
+	table.Add(k+"_1", 100*time.Millisecond, v+"_1")
+	table.Add(k+"_2", 125*time.Millisecond, v+"_2")
+
+	time.Sleep(75 * time.Millisecond)
+
+	_, err := table.Value(k + "_1")
+	if err != nil {
+		t.Error("Error retrieving value from cache:", err)
+	}
+
+	time.Sleep(75 * time.Millisecond)
+
+	_, err = table.Value(k + "_1")
+	if err != nil  {
+		t.Error("Error retrieving value from cache:", err)
+	}
+	_, err = table.Value(k + "_2")
+	if err == nil {
+		t.Error("Found key whitch should have been expired by now")
+	}
+}
+
+func TestExists(t *testing.T) {
+	table := Cache("testExists")
+	table.Add(k, 0, v)
+	if !table.Exists(k) {
+		t.Error("Error verifying data in cache")
+	}
+}
